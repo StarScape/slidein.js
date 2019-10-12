e6019e0e7c846feded1cf59799afdfd5f975d143
Jack <mail@jackarrington.com>
2019-10-12 17:39:16 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const DEFAULT_ATTRS = {
+  ['slide-anim']: 'fadeleft',
+  ['slide-delay']: 0,
+}
+
+const isVisible = (el) => {
+  const rect = el.getBoundingClientRect()
+  const elemTop = rect.top
+  const elemBottom = rect.bottom
+
+  // Only completely visible elements return true:
+  const isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight)
+  // Partially visible elements return true:
+  // const isVisible = elemTop < window.innerHeight && elemBottom >= 0
+  return isVisible;
+}
+
+// Returns an object composed of the properties of `obj` that match the predicate
+// Predicate is passed both and key and value for each property
+const objFilter = (obj, predicate) =>
+   Object.entries(obj)
+         .filter(([key, val]) => predicate(key, val))
+         .reduce((newObj, [key, val]) => (newObj[key] = key, newObj), {})
+
+const getSlideAttributes = (attr) => ({
+  ...DEFAULT_ATTRS,
+  ...objFilter(attr, (key, val) => key.startsWith('slide'))
+})
+
+const revealElements = (slideInElements) => {
+  for (let i = 0; i < slideInElements.length; i++) {
+    const e = slideInElements[i]
+
+    if (isVisible(e) && !e.attributes['_slide-anim-triggered']) {
+      const attrs = getSlideAttributes(e.attributes)
+
+      e.style.visibility             = 'visible'
+      e.style.animation              = `${attrs['slide-anim']} 0.5s ease-in-out`
+      e.style['animation-delay']     = `${attrs['slide-delay']}s`
+      e.style['animation-fill-mode'] = 'forwards'
+
+      // Mark as animated
+      e.setAttribute('_slide-anim-triggered', 'true')
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const slideInElements = Array.from(document.getElementsByClassName('slide'))
+  const onScroll = () => revealElements(slideInElements) 
+  window.addEventListener('scroll', onScroll)
+  onScroll()
+})
